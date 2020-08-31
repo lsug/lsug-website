9d45cf9a381dad998e582d4c1b1f3eb91c01b232
zainab-ali <zainab.ali.london@gmail.com>
2020-08-31 20:35:47 +0100

    Fix link styling for speakers with blog but no picture

---
+++ server/src/main/resources/events/2020-06-16.pm
@@ -0,0 +1,42 @@
+◊meetup{270722214}
+◊hosts{zainabali}
+◊date{2020-06-16}
+◊time{18:30-20:30}
+◊welcome{◊p{
+We look forward to seeing you at another round of London Scala Talks.
+}}
+
+◊items{
+◊item{
+◊name{Type class derivation in Scala 3}
+◊speakers{chrisbirchall}
+◊tags{functional,scala-3}
+◊time{19:00-19:40}
+◊slides{◊url{https://slides.com/cb372/type-class-derivation-in-scala-3/embed}}
+◊description{
+◊p{Type class derivation is one of the most exciting new features of Scala 3. In this talk I’ll explain how library authors can use it to make life easier for their users.
+}
+◊p{
+I’ll start with a quick recap of how type class derivation is typically achieved in Scala 2, and how it looks in other languages such as Haskell and Rust.
+}
+◊p{
+Then I’ll live-code a ◊em{derived} method for the world’s simplest type class, Show. Once we’ve seen how all the pieces fit together, we can move on to a more interesting example: deriving Functor for paramterized ADTs.
+}
+◊p{
+Finally I’ll demonstrate how I’m planning to combine type class derivation with Scala 3’s new macro framework to allow automatic gRPC server and client generation in ◊link{◊url{
+https://higherkindness.io/mu-scala/}◊text{Mu}}, a microservices framework developed by 47 Degrees.
+}
+}
+}
+◊item{
+◊name{Microservices Patterns with Akka and Kafka}
+◊speakers{aleksandarskrbic}
+◊tags{akka,kafka}
+◊time{19:50-20:30}
+◊description{
+◊p{Talk about some common patterns specific to microservices and streaming architectures.}
+◊p{I will show how to integrate Kafka in your microservices and why Akka and especially Akka Streams are great tool for that. Also, I will code a few Akka based services and show how interservice communication can be implemented with Akka Http or Alpakka Kafka.}
+◊p{Finally, I will review our Akka and Kafka based architecture and of course code will be available on GitHub.}
+}
+}
+}
