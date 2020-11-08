b31b8de97fd1ba89381c9d0751c0e4a8ecb93b2b
Zainab Ali <zainab.ali.london@gmail.com>
2020-11-08 23:12:55 +0000

    Support events with no speakers (#26)

--- server/src/main/resources/meetups/2020-11-28.pm
+++ server/src/main/resources/meetups/2020-11-28.pm
@@ -12,7 +12,38 @@
 ◊time{14:00-17:30}
 ◊setup{◊p{Please run through the ◊link{◊text{contributor’s guide}◊url{https://github.com/lsug/lsug-website/blob/master/CONTRIBUTING.md}} to get set up.}}
 ◊description{
-◊p{Do you want to contribute to open source, but don’t know how to start? There’s no better place than the new ◊link{◊text{London Scala User Group website}◊url{https://github.com/lsug/lsug-website/}}.} 
+◊p{Do you want to contribute to open source, but don’t know how to start? There’s no better place than the new ◊link{◊text{London Scala User Group website}◊url{https://github.com/lsug/lsug-website/issues?q=is%3Aissue+is%3Aopen+label%3A%22good+first+issue%22}}.} 
 ◊p{From the http4s webservice to the ScalaJS frontend, there are plenty of areas that are waiting for your help. Developers of all experiences are welcome. Don’t be nervous if it’s you’re first time contributing, we’re here to walk you through the GitHub PR process.}
 }}
-}
+◊event{
+◊name{The Scala Language}
+◊speakers{chrisbirchall}
+◊tags{open-source}
+◊time{14:00-17:30}
+◊setup{◊p{Please run through the ◊link{◊text{contributor’s guide}◊url{https://github.com/scala/scala/blob/2.13.x/CONTRIBUTING.md}} to get set up.}}
+◊description{
+◊p{Make your first pull request to the ◊link{◊text{Scala language}◊url{https://github.com/scala/scala}}}
+}}
+◊event{
+◊name{ScalaJS}
+◊speakers{sebastiendoeraene}
+◊tags{open-source}
+◊time{14:00-17:30}
+◊setup{◊p{Please run through the ◊link{◊text{developer’s guide}◊url{https://github.com/scala-js/scala-js/blob/master/DEVELOPING.md}} to get set up.}}
+◊description{
+◊p{Make your first pull request to ◊link{◊text{ScalaJS}◊url{https://github.com/scala-js/scala-js}}}
+}}
+◊event{
+◊name{The Typelevel ecosystem}
+◊tags{open-source}
+◊time{14:00-17:30}
+◊description{
+◊p{Make your first pull request in  the ◊link{◊text{Typelevel ecosystem}◊url{https://typelevel.org/projects/}}}
+}}
+◊event{
+◊name{And many more!}
+◊tags{open-source}
+◊time{14:00-17:30}
+◊description{
+◊p{There are a wealth of projects in need of new contributors.  Join us to find out more.}
+}}}
