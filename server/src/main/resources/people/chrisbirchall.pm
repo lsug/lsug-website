b118263f0a5d0229312c636b86e6ff1ac8f94aeb
Chris Birchall <chris.birchall@gmail.com>
2020-11-16 23:34:55 +0000

    Chris Birchall bio and event (#37)
    * Add photo and bio for Chris Birchall
    
    * Flesh out the event details
    
    * Try bumping the setup-mill action
    
    * Opt in to unsecure command execution
    
    * Remove trailing brace
    
    * Crop photo (until we fix the CSS)
    
    Co-authored-by: zainab-ali <zainab.ali.london@gmail.com>

--- server/src/main/resources/people/chrisbirchall.pm
+++ server/src/main/resources/people/chrisbirchall.pm
@@ -1,5 +1,9 @@
 ◊name{Chris Birchall}
+◊photo{chrisbirchall.png}
 ◊social{
   ◊twitter{cbirchall}
   ◊github{cb372}
+}
+◊bio{
+  ◊p{Chris has been writing Scala since 2009. He (sporadically) maintains a number of open source projects including ◊link{◊text{cats-retry}◊url{https://github.com/cb372/cats-retry}}, ◊link{◊text{sbt-explicit-dependencies}◊url{https://github.com/cb372/sbt-explicit-dependencies}} and ◊link{◊text{scala-typed-holes}◊url{https://github.com/cb372/scala-typed-holes}}.
 }
