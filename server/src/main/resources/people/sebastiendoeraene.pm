5d86e0c920a47af15befd6ec4bb43318ab08dc7c
Sébastien Doeraene <sjrdoeraene@gmail.com>
2020-11-26 17:31:04 +0100

    Fix #33: Fill in sjrd's bio and the Scala.js sub-event.

--- server/src/main/resources/people/sebastiendoeraene.pm
+++ server/src/main/resources/people/sebastiendoeraene.pm
@@ -1,4 +1,9 @@
 ◊name{Sébastien Doeraene}
+◊photo{sebastiendoeraene.png}
 ◊social{
-◊github{sjrd}
+  ◊github{sjrd}
+  ◊twitter{sjdoeraene}
+}
+◊bio{
+  ◊p{Sébastien Doeraene is a compiler/runtime systems hacker and a Scala enthusiast. Best known as the author of ◊link{◊text{Scala.js}◊url{https://www.scala-js.org/}}, he is now technical director of the ◊link{◊text{Scala Center}◊url{https://scala.epfl.ch/}}. He holds a PhD from EPFL, having worked under the supervision of Prof. Martin Odersky, and a master's degree in computer science engineering from Université catholique de Louvain in Belgium. When he is not busy coding, he sings in choirs and a cappella groups such as the ◊link{◊text{Ensemble Vocal Évohé}◊url{http://www.ensemble-vocal-evohe.com/}}, or composes for said choirs.}
 }
