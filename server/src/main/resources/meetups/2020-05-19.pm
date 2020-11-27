98e8731073c16e50cea844eb2f96cb8ca34b1873
zainab-ali <zainab.ali.london@gmail.com>
2020-11-27 20:23:02 +0000

    Add meetup for May

---
+++ server/src/main/resources/meetups/2020-05-19.pm
@@ -0,0 +1,38 @@
+◊meetup{270419868}
+◊hosts{zainabali}
+◊date{2020-05-19}
+◊time{18:30-20:00}
+◊welcome{◊p{
+We look forward to seeing you at another round of London Scala Talks.
+}}
+
+◊events{
+◊event{
+◊name{War stories of lighting a Spark in the Kubernetes sea}
+◊speakers{roksolanadiachuk}
+◊tags{Scala,Kubernetes}
+◊recording{JwOM3GtqLsQ}
+◊time{19:00-19:40}
+◊slides{◊url{https://speakerdeck.com/roksolanad/war-stories-of-lighting-a-spark-in-the-kubernetes-sea}◊external{t}}
+◊description{
+◊p{
+Big data, Spark, Kubernetes — you may have heard these buzzwords far too many times. Have you ever thought of combining all of them? Running spark jobs on Kubernetes — sounds intriguing! But sailing the unknown see always goes with a challenge, and not even one. And who knows what is hiding deep in the water? If you are ready, let’s embark on this dangerous yet exciting journey. In this talk, I am going to talk about new Spark operator for Kubernetes which allows to run Spark jobs on Kubernetes in a more automated way. The audience will find out about all the challenges my team and I encountered while researching this possibility. How we came all the way through using Spark operator alpha version which then got updated to beta (you know, updates). How we dealt with problems and challenges we encountered on our way.
+}
+}
+}
+◊event{
+◊name{FP + relational databases = ?}
+◊speakers{adamwarski}
+◊tags{Scala,functional,database}
+◊recording{1QwbWVJ_Dgg}
+◊time{19:50-20:30}
+◊material{
+◊link{◊url{https://github.com/adamw/sql-fp-pres}◊text{sql-fp-pres repository}}
+◊link{◊url{https://blog.softwaremill.com/from-transactional-to-type-safe-reasonable-transactions-a5019906245e}◊text{From @Transactional to type-safe, reasonable transactions}}
+◊link{◊url{https://softwaremill.com/scala/}◊text{SoftwareMill on Scala}}
+}
+◊description{
+◊p{Functional programming is an increasingly popular approach to writing and thinking about code. Is it also applicable to such daily programming tasks as accessing a relational database? Is it possible to work with transactions in a “functional” way? Yes! But what does it mean in practice? And is this any better than using JPA’s or Spring’s @Transactional?}
+◊p{In this live-coding session, we'll try to answer these questions, and demonstrate how the “functional” approach improves over the “traditional” one in three areas: working with multi‑thread code, requiring a framework and precisely delimiting transaction boundaries. In the code we’ll use the Doobie library, however no prior knowledge of Doobie is necessary. In fact, you should be able to follow the talk even if you don’t know Scala!}
+◊p{We’ll be focusing on one specific problem area, however the techniques that we’ll use have broader applicability, and are often used to solve other problems as well.}
+}}}
---
+++ server/src/main/resources/people/adamwarski.pm
@@ -0,0 +1,14 @@
+◊name{Adam Warski}
+◊photo{adam-warski.jpeg}
+◊social{
+◊github{adamw}
+◊blog{https://softwaremill.com/blog}
+◊twitter{adamwarski}
+}
+◊bio{◊p{
+I am one of the co-founders of SoftwareMill, where I code mainly using Scala and other interesting technologies. I am involved in open-source projects, such as sttp, tapir, Quicklens, ElasticMQ and others. I have been a speaker at major conferences, such as JavaOne, LambdaConf, Devoxx and ScalaDays.
+}
+◊p{
+Apart from writing closed- and open-source software, in my free time I try to read the Internet on various (functional) programming-related subjects. Any ideas or insights usually end up with a ◊link{◊url{https://softwaremill.com/blog}◊text{blog}}.
+}
+}
