867aa215042d21c1cbf458afafe418910d53328e
Zainab Ali <zainab.ali.london@gmail.com>
2020-11-27 20:42:35 +0000

    Add meetup for May (#52)
    * Add meetup for May
    
    * Add video for last November

---
+++ server/src/main/resources/people/roksolanadiachuk.pm
@@ -0,0 +1,8 @@
+◊name{Roksolana Diachuk}
+◊photo{roksolana-diachuk.jpg}
+◊social{
+◊twitter{dead_flowers22}
+◊github{roksolana-d}
+}
+◊bio{◊p{Roksolana works as Big Data Engineer at Captify. She is passionate about Big Data, Scala, and Kubernetes and she always loves to learn something new. Roksolana is one of the Women Who Code Kyiv leads so she enjoys organizing events as well as speaking at them. Her hobbies are building technical topics around fairytales and discovering new cities by traveling.
+}}
