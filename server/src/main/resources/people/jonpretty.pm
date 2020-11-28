c326e544119a2d585b920471e4159d3905303724
Jon Pretty <jon.pretty@propensive.com>
2020-11-28 13:54:46 +0100

    Added `jonpretty` and Probably project

---
+++ server/src/main/resources/people/jonpretty.pm
@@ -0,0 +1,8 @@
+◊name{Jon Pretty}
+◊social{
+  ◊twitter{propensive}
+  ◊github{propensive}
+}
+◊bio{
+◊p{Jon is an open-source developer, advisor to the VirtusLab steering committee, and professional trainer.}
+}
