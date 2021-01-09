2c0f67b2dbf53cb8c8aa0174d67c8ca079efd14a
zainab-ali <zainab.ali.london@gmail.com>
2021-01-09 13:34:29 +0000

    Update issue template for 28th Jan

---
+++ server/src/main/resources/meetups/2021-01-28.pm
@@ -0,0 +1,10 @@
+◊meetup{275644800}
+◊hosts{zainab-ali}
+◊date{2021-01-28}
+◊time{18:00-19:30}
+◊welcome{◊p{
+We look forward to seeing you at another round of London Scala Talks.
+}}
+
+◊events{
+}
