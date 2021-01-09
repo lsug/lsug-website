0aa4f0fbd2b1eaa6a857706b975f8ba9e0b0d960
Nicolas Rinaudo <nicolas.rinaudo@besedo.com>
2021-01-09 21:54:06 +0100

    Add bio and event for Nicolas Rinaudo

--- server/src/main/resources/meetups/2021-01-28.pm
+++ server/src/main/resources/meetups/2021-01-28.pm
@@ -7,4 +7,25 @@ We look forward to seeing you at another round of London Scala Talks.
 }}
 
 ◊events{
+◊event{
+◊name{Introduction to recursion schemes}
+◊speakers{nicolasrinaudo}
+◊tags{functional}
+◊time{18:00-19:30}
+◊slides{◊url{https://nrinaudo.github.io/recursion-schemes-from-the-ground-up/}}
+◊material{
+◊link{◊text{Article}◊url{https://nrinaudo.github.io/recschemes/}}
+}
+◊description{
+◊p{
+Recursion schemes are a topic that often come up in conversations with experienced, knowledgeable developers. For the longest time, I'd just nod and smile and pretend I knew what was going on. The purpose of this talk is to help people in the same situation. Recursion schemes are far less scary and confusing than the mystique that surrounds them suggests, and this talk attempts to prove it.
+}
+◊p{
+We'll start from first principles and refactor our way to proper recursion schemes, discovering and explaining all the necessary concepts as we go along.
+}
+◊p{
+By the end of the talk, attendants should have all the tools they need to understand recursion schemes, and be able to explore the more esoteric ones on their own.
+}
+}
+}
 }
