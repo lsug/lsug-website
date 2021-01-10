231d98465ba5cb5deb3e6adfc5c57ec33d11f162
zainab-ali <zainab.ali.london@gmail.com>
2021-01-10 16:14:23 +0000

    Use curly quote instead of apostrophe

--- server/src/main/resources/people/nicolasrinaudo.pm
+++ server/src/main/resources/people/nicolasrinaudo.pm
@@ -9,7 +9,7 @@
 Nicolas is the CTO of Besedo, where we use Scala to moderate large volumes of user generated content and do weird things to machine learning models.
 }
 ◊p{
-After too many years as a Java programmer and a thankfully brief stint in marketing, Nicolas discovered Functional Programming through Scala and fell in love. Since then, he's made it his mission to learn and explain the scary bits, by focusing on practical applications.
+After too many years as a Java programmer and a thankfully brief stint in marketing, Nicolas discovered Functional Programming through Scala and fell in love. Since then, he’s made it his mission to learn and explain the scary bits, by focusing on practical applications.
 }
 ◊p{
 Nicolas is also the author and sole maintainer of a few useful OSS libraries, such as kantan.csv.
