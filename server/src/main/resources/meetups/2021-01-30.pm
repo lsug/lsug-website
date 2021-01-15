f958f35d65ba3ed746e9abd3eff19b5e95fcfa17
Zainab Ali <zainab.ali.london@gmail.com>
2021-01-15 18:10:37 +0000

    Add January OSS Saturday (#93)

---
+++ server/src/main/resources/meetups/2021-01-30.pm
@@ -0,0 +1,27 @@
+◊meetup{274890895}
+◊hosts{zainabali}
+◊date{2020-01-30}
+◊time{14:00-17:30}
+◊welcome{◊p{We look forward to seeing you at the London Scala Open Source Saturday.}}
+◊events{
+◊event{
+◊name{Contribute to the LSUG website}
+◊speakers{zainabali}
+◊tags{open-source,Scala, ScalaJS}
+◊time{14:00-17:30}
+◊setup{◊p{Please run through the ◊link{◊text{contributor’s guide}◊url{https://github.com/lsug/lsug-website/blob/master/CONTRIBUTING.md}} to get set up.}}
+◊description{
+◊p{
+Do you want to contribute to open source, but don’t know how to
+start? There’s no better place than the new
+◊link{◊text{London Scala User Group
+website}◊url{https://github.com/lsug/lsug-website/issues?q=is%3Aissue+is%3Aopen+label%3A%22good+first+issue%22}}.
+}
+◊p{
+From the http4s webservice to the ScalaJS frontend, there are
+plenty of areas that are waiting for your help. Developers of all
+experiences are welcome. Don’t be nervous if it’s you’re first time
+contributing, we’re here to walk you through the GitHub PR process.
+}
+}}
+}
