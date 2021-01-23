aace99a14ead76200e49d491d2bd79cfed453587
Adam Fraser <adam.fraser@gmail.com>
2021-01-22 19:13:48 -0800

    Add Bio and Event (#100)
    * add bio and event
    
    * Remove trailing brace
    
    Co-authored-by: zainab-ali <zainab.ali.london@gmail.com>

---
+++ server/src/main/resources/people/adamfraser.pm
@@ -0,0 +1,9 @@
+◊name{Adam Fraser}
+◊photo{adamfraser.jpg}
+◊social{
+  ◊twitter{adamfraser}
+  ◊github{adamgfraser}
+}
+◊bio{
+◊p{Adam Fraser is a software engineer passionate about using the power of functional programming to solve hard business problems. He is one of the top contributors to ZIO, a library for asynchronous and concurrent programming in functional Scala. Adam is the co-author of the ZIO Test and the author of ZIO Query. He is a regular conference speaker, writer, and mentor to other software developers regarding functional programming.}
+}
