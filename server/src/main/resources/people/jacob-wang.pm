0f07545962c7e918f8808ed478a4f6b702eadec7
zainab-ali <zainab.ali.london@gmail.com>
2021-02-16 22:22:49 +0000

    Use curly apostrophe

--- server/src/main/resources/people/jacob-wang.pm
+++ server/src/main/resources/people/jacob-wang.pm
@@ -5,5 +5,5 @@
   ◊github{jatcwang}
 }
 ◊bio{
-◊p{Jacob is a software developer at Medidata. He is passionate about better ways to solve problems with software, whether that's functional programming, better tools or a cat on his lap.}
+◊p{Jacob is a software developer at Medidata. He is passionate about better ways to solve problems with software, whether that’s functional programming, better tools or a cat on his lap.}
 }
