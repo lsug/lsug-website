107ddb10da378df29400601da220e63310adf143
Noel Welsh <noel@noelwelsh.com>
2021-02-27 13:32:33 +0000

    Add ScalaBridge event content (#128)
    * Add ScalaBridge event content
    
    * Add an extra parenthesis
    
    Co-authored-by: zainab-ali <zainab@kebab-ca.se>

---
+++ server/src/main/resources/people/noelwelsh.pm
@@ -0,0 +1,10 @@
+◊name{Noel Welsh}
+◊photo{noelwelsh.png}
+◊social{
+  ◊twitter{noelwelsh}
+  ◊github{noelwelsh}
+  ◊blog{https://noelwelsh.com/}
+}
+◊bio{
+  ◊p{Noel is a Scala consultant working at ◊link{◊text{Inner Product}◊url{https://inner-product.com/}, where he helps teams achieve more with Scala.}}
+}
